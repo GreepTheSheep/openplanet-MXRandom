beae1cac565da16c8abff9aeb46fc5a75379aed0
Matthieu Dehon <42576124+GreepTheSheep@users.noreply.github.com>
2022-06-06 21:20:28 +0200

    Update build-op.example.bat

--- build-op.example.bat
+++ build-op.example.bat
@@ -1,5 +1,14 @@
+:: https://gist.github.com/GreepTheSheep/d95921bc9cb10287c39611bd429d9273
+
 @echo off
 
 :: Set here your path to 7-Zip, including 7z.exe
 SET zip="C:\Program Files\7-Zip\7z.exe"
-%zip% a -mx1 -tzip MXRandom.op info.toml src
+
+:: This will get the current directory name
+for %%I in (.) do SET CurrDirName=%%~nxI
+
+IF EXIST %CurrDirName%.op (
+    del %CurrDirName%.op
+)
+%zip% a -mx1 -tzip %CurrDirName%.op info.toml src
